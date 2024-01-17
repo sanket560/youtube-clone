781cc3d6e0aed8da32eacb6e7cacb24bc6182fc0
sanket mane <sanketmmane@gmail.com>
2024-01-18 01:05:00 +0530

    first commit

---
+++ src/utils/api.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "IN" },
+    headers: {
+      "X-RapidAPI-Key":import.meta.env.VITE_API_KEY,
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
